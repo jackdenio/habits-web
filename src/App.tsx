2b0db85fa43f9a3149fc57171ac5f32d9f5c8905
jackdenio <deniogomes@gmail.com>
2023-03-05 15:45:45 -0300

    Create Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { Habit } from "./assets/components/Habit";
+//import { Habit } from "./assets/components/Habit";
 import './styles/global.css';
+import logoImage from './assets/logo.svg'
+import { Plus } from 'phosphor-react'
 
-function App() {
+export function App() {
   
 
   return (
-    <div className="bg-slate-800">
-      <Habit completed={8} />
-      <Habit completed={10} />      
+    <div className="w-screen h-screen flex justify-center items-center max-sm:items-start max-sm:mt-16">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <div className="w-full max-w-3xl mx-auto flex  items-center justify-between max-sm:flex-col max-sm:gap-5 ">
+          <img src={logoImage} alt="Habits"></img>
+          <button
+            type="button"
+            className="border border-yellow5 font-semibold rounded-lg px-6 py-2 flex items-center gap-3 hover:border-yellow2"
+          >
+            <Plus size={20} className="text-yellow5"></Plus>
+            Novo Hábito
+          </button>
+        </div>
+
+      </div>
     </div>
   )
 }
-
-export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,20 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
-    './index.html'
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#010304',
+        yellow1: '#e47200',
+        yellow2: '#e69b00',
+        yellow3: '#e6b400',
+        yellow4: '#e6cc00',
+        yellow5: '#e5de00',
+      },
+      
+    },
   },
   plugins: [],
 }
